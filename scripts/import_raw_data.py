a01292f258fbc3ac563006605ed0cbc42a8c1bb4
Prescila Mora <prescilafmora@gmail.com>
2024-06-25 09:50:59 -0400

    update import books data

--- scripts/import_raw_data.py
+++ scripts/import_raw_data.py
@@ -39,30 +39,34 @@ try:
     # Initialize BlobServiceClient
     blob_service_client = BlobServiceClient.from_connection_string(azure_storage_connection_string)
     print("Connected to Azure Blob Storage.")
-    sys.exit() 
-    # Read CSV files from Azure Blob Storage into pandas DataFrames
-    container_name = "nlpdata"
-    reviews_blob_name = "Books_rating.csv"
-    books_blob_name = "books_data.csv"
-
-    reviews_df = read_blob_to_dataframe(blob_service_client, container_name, reviews_blob_name)
-    print("Successfully read reviews data from blob storage.")
-    books_df = read_blob_to_dataframe(blob_service_client, container_name, books_blob_name)
-    print("Successfully read books data from blob storage.")
-
+    
+    
     # Initialize MongoDB client
     client = MongoClient(mongo_connection_string)
     db = client['AmazonBooksReviews']
     print("Connected to Azure Cosmos DB.")
 
-    # Insert data into MongoDB collections
-    reviews_collection = db['raw_reviews']
+    
+    # Read CSV files from Azure Blob Storage into pandas DataFrames
+    container_name = "nlpdata"
+    reviews_blob_name = "Books_rating.csv"
+    books_blob_name = "books_data.csv"
+    
+    books_df = read_blob_to_dataframe(blob_service_client, container_name, books_blob_name)
+    print("Successfully read books data from blob storage.")
+    sys.exit()    
     books_collection = db['raw_books']
-
-    reviews_collection.insert_many(reviews_df.to_dict(orient='records'))
-    print("Successfully inserted reviews data into MongoDB.")
+    
     books_collection.insert_many(books_df.to_dict(orient='records'))
     print("Successfully inserted books data into MongoDB.")
+    
+    reviews_df = read_blob_to_dataframe(blob_service_client, container_name, reviews_blob_name)
+    print("Successfully read reviews data from blob storage.")
+    # Insert data into MongoDB collections
+    reviews_collection = db['raw_reviews']
+    
+    reviews_collection.insert_many(reviews_df.to_dict(orient='records'))
+    print("Successfully inserted reviews data into MongoDB.")
 
     print("Data import completed successfully.")
 except Exception as e:
